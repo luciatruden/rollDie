45d3c779f6c3fd74166f265a6fbda05701a1593b
Lucia Trujillo <ltdennis@gmail.com>
2022-11-06 17:58:28 +0100

    add new isRolling state to RollDie
    toggle on/off inside RollDie() with 1s timeout
    disable button while rolling

--- src/RollDice.js
+++ src/RollDice.js
@@ -13,11 +13,20 @@ function RollButton(props){
 
     const [die1, setDie1] = useState(rollDie());
     const [die2, setDie2] = useState(rollDie());
+    const [isRolling, setIsRolling] = useState(false);
 
     function rollButton() {
+        setIsRolling(true);
         setDie1(rollDie());
         setDie2(rollDie());
+
+        setTimeout(()=> setIsRolling(false), 1000);
     }
+    const buttonLabel = isRolling? 'Rolling...' : 'Roll dice';
+
+    const buttonElement = <button className='RollDice-button' onClick={ rollButton } disabled={isRolling}> { buttonLabel }</button>;
+    const winMsg = <h2 className="RollDice-win">You win!</h2>;
+
 
     return(
         <div className="RollDice">
@@ -25,7 +34,7 @@ function RollButton(props){
                 <Dice roll={die1} />
                 <Dice roll={die2}/>
             </div>
-            <button className="RollDice-button" onClick={ rollButton } >Roll dice</button>
+            { die1 === die2? winMsg : buttonElement}
         </div>
         
     );

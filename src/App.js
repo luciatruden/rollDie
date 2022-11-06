1bddd77ae63e589942c92acad070b47ea63a8083
Lucia Trujillo <ltdennis@gmail.com>
2022-11-06 16:32:38 +0100

    add Dice and RollButton calls in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import RollButton from './RollButton';
+import Dice from './Dice';
 
 function App() {
   return (
     <div className="App">
-      
+      <Dice />
+      <RollButton />
     </div>
   );
 }
---
+++ src/RollButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+import './RollButton';
+
+function RollButton(props){
+
+
+}
+
+export default RollButton;

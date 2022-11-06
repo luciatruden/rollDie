27ca4dd89217c2898240b2d7a4a137e4cfd7c014
Lucia Trujillo <ltdennis@gmail.com>
2022-11-06 16:59:04 +0100

    add rollDie function to Die.js to build url to die icon

--- src/Dice.js
+++ src/Dice.js
@@ -3,7 +3,19 @@ import './Dice.css';
 
 function Dice(props){
 
+    function rollDie() {
+        const rolls = ['one', 'two', 'three', 'four', 'five', 'six'];
+        let index = Math.floor(Math.random()*6);
+        return rolls[index];
+    }
 
+    let rollClassName = `fas fa-dice-${rollDie()}`;
+    
+    return (
+        <div className="Dice">
+            <i className={rollClassName}></i>
+        </div>
+    );
 }
 
 export default Dice;

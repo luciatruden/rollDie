a42ad7b68d8b1a7e99a5081fbe32131a6ffda29e
Lucia Trujillo <ltdennis@gmail.com>
2022-11-06 17:26:50 +0100

    add rollButton function to update status of both dice on click of button

--- src/RollDice.js
+++ src/RollDice.js
@@ -14,14 +14,18 @@ function RollButton(props){
     const [die1, setDie1] = useState(rollDie());
     const [die2, setDie2] = useState(rollDie());
 
-    
+    function rollButton() {
+        setDie1(rollDie());
+        setDie2(rollDie());
+    }
+
     return(
         <div className="RollDice">
             <div className='RollDice-die'>
                 <Dice roll={die1} />
                 <Dice roll={die2}/>
             </div>
-            <button className="RollDice-button">Roll dice</button>
+            <button className="RollDice-button" onClick={ rollButton } >Roll dice</button>
         </div>
         
     );
